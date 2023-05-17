ddb391216e07e74b0f82a52368ac304eed173142
Sydiaka32 <max.sydiaka@gmail.com>
2023-05-17 11:17:49 +0300

    Adding foreign key

--- Classes.sql
+++ Classes.sql
@@ -6,3 +6,10 @@ Resourse varchar(255),
 RangeType varchar(255),
 PRIMARY KEY (ClassID)
 );
+
+ALTER TABLE Classes
+ADD ChampionID int;
+
+ALTER TABLE Classes
+ADD CONSTRAINT fk_Champions
+FOREIGN KEY (ChampionID) REFERENCES Champions(ID);
