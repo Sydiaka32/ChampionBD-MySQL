e5f38680f39c6b6579c48df3bf437f20a7f8fe67
Sydiaka32 <max.sydiaka@gmail.com>
2023-05-17 11:14:10 +0300

    Creating Classes table

---
+++ Classes.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Classes (
+ClassID int NOT NULL AUTO_INCREMENT,
+ChampionName varchar(255),
+ChampType varchar(255),
+Resourse varchar(255),
+RangeType varchar(255),
+PRIMARY KEY (ClassID)
+);
