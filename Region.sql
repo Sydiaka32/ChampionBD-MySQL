ebddb700a77a8939cde79d9c9c6a40f8a64d1d17
Sydiaka32 <max.sydiaka@gmail.com>
2023-05-18 14:49:07 +0300

    Adding region and relashionship tables

---
+++ Region.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Regions (
+RegionID int NOT NULL AUTO_INCREMENT,
+Region varchar(255),
+PRIMARY KEY (RegionID)
+);
+
+INSERT INTO Regions (Region)
+VALUES ('Shurima'),
+('Ionia'),
+('Targon'),
+('Zaun'),
+('Freljord'),
+('Shadow Isles'),
+('Void'),
+('Noxus'),
+('Demasia'),
+('Bandle City'),
+('Bilgewater'),
+('Piltover'),
+('Ixtal');
+
+INSERT INTO Regions (Region)
+VALUES ('Runeterra');
